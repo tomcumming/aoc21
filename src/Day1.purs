53b81d06f62e4d9fd7447c19dee808adc71fd38f
Tom Cumming <mail@tomcumming.co.uk>
2021-12-01 19:41:17 +0000

    Add window function

--- src/Day1.purs
+++ src/Day1.purs
@@ -1,30 +1,35 @@
-module Day1 (readLines, pairs, day1) where
+module Day1 (readLines, window, day1) where
 
 import Prelude
 
 import Data.Int (fromString)
-import Data.List.Lazy (List, Step(..), cons, filter, fromFoldable, length, nil, step)
+import Data.List.Lazy (List, Step(..), cons, filter, fromFoldable, length, nil, step, take)
 import Data.Maybe (Maybe(..))
 import Data.String (Pattern(..), split, trim)
 import Data.Traversable (sequence)
-import Data.Tuple (Tuple(..))
 
 readLines :: String -> List String
 readLines = filter (_ /= "") <<< map trim <<< fromFoldable <<< split (Pattern "\n")
 
-pairs :: forall a. List a -> List (Tuple a a)
-pairs xs = go Nothing xs
+window :: forall a. Int -> List a -> List (List a)
+window n xs = filter (\xs -> length xs >= n) $ go nil xs
   where
-    go :: Maybe a -> List a -> List (Tuple a a)
-    go h xs = case Tuple h (step xs) of
-      Tuple _ Nil -> nil
-      Tuple Nothing (Cons x xs) -> go (Just x) xs
-      Tuple (Just l) (Cons x xs) -> cons (Tuple l x) $ go (Just x) xs
+    go :: List a -> List a -> List (List a)
+    go xs ys = case step ys of
+      Nil -> nil
+      Cons y ys ->
+        let xs' = cons y xs
+        in cons (take n xs') $ go xs' ys
 
-depthIncreases :: Tuple Int Int -> Boolean
-depthIncreases (Tuple a b) = a < b
+depthIncreases :: List Int -> Maybe Boolean
+depthIncreases xs = case step xs of
+  Cons x xs -> case step xs of
+    Cons y _ -> Just (x > y)
+    _ -> Nothing
+  _ -> Nothing
 
 day1 :: String -> Maybe Int
 day1 inputStr = do
   inputs <- sequence $ map fromString $ readLines inputStr
-  pure $ length $ filter depthIncreases $ pairs inputs
+  increases <- sequence $ map depthIncreases $ window 2 inputs
+  pure $ length $ filter (\x -> x) $ increases

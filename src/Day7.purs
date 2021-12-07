9c9ffffa432f057f9541646e6655604eca74ba8f
Tom Cumming <mail@tomcumming.co.uk>
2021-12-07 18:20:46 +0000

    Day 7 pt 2

--- src/Day7.purs
+++ src/Day7.purs
@@ -1,4 +1,4 @@
-module Day7 (score, day7) where
+module Day7 (score, score2, day7, day7_2) where
 
 import Prelude
 
@@ -10,9 +10,19 @@ import Data.Ord (abs)
 score :: Array Int -> Int -> Int
 score xs n = foldl (\a b -> a + abs (b - n)) 0 xs
 
+score2 :: Array Int -> Int -> Int
+score2 xs n = foldl (\a b -> let c = abs (b - n) in a + (c * (c + 1)) / 2) 0 xs
+
 day7 :: Array Int -> Maybe Int
 day7 xs = do
   min <- minimum xs
   max <- maximum xs
   n <- minimumBy (comparing (score xs)) (range min max)
   Just $ score xs n
+
+day7_2 :: Array Int -> Maybe Int
+day7_2 xs = do
+  min <- minimum xs
+  max <- maximum xs
+  n <- minimumBy (comparing (score2 xs)) (range min max)
+  Just $ score2 xs n
--- test/Test/Day7.purs
+++ test/Test/Day7.purs
@@ -1,9 +1,9 @@
-module Test.Day7 (testDay7, testScore) where
+module Test.Day7 (testDay7, testDay7_2, testScore) where
 
 import Prelude
 
 import Data.Maybe (Maybe(..))
-import Day7 (score, day7)
+import Day7 (day7, day7_2, score)
 import Effect (Effect)
 import Effect.Console (log)
 import Test (showResult)
@@ -19,3 +19,9 @@ testDay7 :: Effect Unit
 testDay7 = do
   log $ "Day 7: "
     <> showResult (day7 [16,1,2,0,4,2,7,1,2,14] == Just 37)
+
+
+testDay7_2 :: Effect Unit
+testDay7_2 = do
+  log $ "Day 7: "
+    <> showResult (day7_2 [16,1,2,0,4,2,7,1,2,14] == Just 206)
